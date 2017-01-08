9916af941f7701e3357b084f133c240eb36a024a
sytlnrf <sytlnrf@gmail.com>
2017-01-08 21:06:17 +0800

    rm .pyc

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ from deps import depsvm
 if __name__ == "__main__":
     PRO_DIR = config.get_project_dir()
     feature_arr, label_arr = utils.load_data_ml(PRO_DIR + "/data/testSet.txt")
-    depsvm.plot_dots_hyper_lane_2(feature_arr, label_arr)
+    depsvm.plot_dots_hyper_lane_2(feature_arr, label_arr, None, None)
     svm_classifer = classifer.Svm()
     svm_classifer.load_data(feature_arr, label_arr)
     svm_classifer.train()
