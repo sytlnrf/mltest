4e24afc1c31de9e4a0d5fe665687fe7b85f5bbc5
sytlnrf <sytlnrf@gmail.com>
2017-02-12 17:24:00 +0800

    create adaboost demo class

---
+++ src/model/adaboost.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""
+Author: shengyitao
+Create Date: 2017-02-12
+"""
+class AdaboostDemo(object):
+    """
+    demo for adaboost algorithm
+    """
+    def __init__(self):
+        self.name = 'AdaboostDemo'
